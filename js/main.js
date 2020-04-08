b8251e3258a21ea248db9432c6c51084e0262ebf
Gabor Ecker <ecker.gabor@gmail.com>
2020-04-08 22:10:50 +0200

    Upgraded Bootstrap. Improved scrolling of the song dialog's body. Fixed webmanifest.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,14 @@
 require.config({
     paths: {
         jquery: 'lib/jquery.3.3.1.min',
-        bootstrap: 'lib/bootstrap.bundle.4.1.3.min',
+        bootstrap: 'lib/bootstrap.bundle.4.4.1.min',
         mustache: 'lib/mustache.3.0.1.min',
-    },
-    onNodeCreated: function(node, config, module, path) {
-        var sri = {
-            jquery: 'sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=',
-            bootstrap: 'sha256-E/V4cWE4qvAeO5MOhjtGtqDzPndRO1LBk8lJ/PR7CA4=',
-            mustache: 'sha256-srhz/t0GOrmVGZryG24MVDyFDYZpvUH2+dnJ8FbpGi0=',
-        };
-        if (sri[module]) {
-            node.setAttribute('integrity', sri[module]);
-            node.setAttribute('crossorigin', 'anonymous');
-        }
     },
     urlArgs: function(id, url) {
         if (url in emmet_busts) {
             return (url.indexOf('?') === -1 ? '?' : '&') + "bust=" + emmet_busts[url];
         }
         return "";
-    },
-    shim: {
-        audioplayer: {
-            deps: ['jquery'],
-        },
     },
 });
 
