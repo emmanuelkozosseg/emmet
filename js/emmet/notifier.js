54f2328c899505071aa0ef17992b4c1de0c86455
Gabor Ecker <ecker.gabor@gmail.com>
2025-03-01 22:49:45 +0100

    chore: migrate to github

--- js/emmet/notifier.js
+++ js/emmet/notifier.js
@@ -15,7 +15,7 @@ define([], function() {
         showProgramError: function(errorMsg, url, lineNumber, columnNumber) {
             showError("Programhiba",
                     "<p>Az Emmet programhibát észlelt. Érdemes újratölteni az oldalt, mivel a működése instabillá válhat.</p>"+
-                    "<p><small>Ha szeretnél segíteni a javításban, kérünk, <a href=\"https://bitbucket.org/eckerg/emmet/issues\" target=\"_blank\">jelentsd be a hibát</a> " +
+                    "<p><small>Ha szeretnél segíteni a javításban, kérünk, <a href=\"https://github.com/emmanuelkozosseg/emmet/issues\" target=\"_blank\">jelentsd be a hibát</a> " +
                     "a lenti részletekkel együtt, és hogy mit csináltál, mielőtt a hiba előjött volna. <strong>Köszönjük! :)</strong></small></p>"+
                     "<p><code>Error: "+errorMsg+"<br />URL: "+url+"<br />"+
                     "Line number: "+lineNumber+"<br />"+
@@ -29,7 +29,7 @@ define([], function() {
         showSongLoadingError: function(errorDetails) {
             setContents("fatal", "Énekbetöltési hiba",
                     "<p>Az Emmet nem tudja betölteni az énekeket, ezért sajnos nem működőképes. :(</p>"+
-                    "<p><small>Ha szeretnél segíteni a javításban, kérünk, <a href=\"https://bitbucket.org/eckerg/emmet/issues\" target=\"_blank\">jelentsd be a hibát</a> " +
+                    "<p><small>Ha szeretnél segíteni a javításban, kérünk, <a href=\"https://github.com/emmanuelkozosseg/emmet/issues\" target=\"_blank\">jelentsd be a hibát</a> " +
                     "a lenti részletekkel együtt. <strong>Köszönjük! :)</strong></small></p>"+
                     "<p><code>"+errorDetails.replace(/\n/g, "<br />")+"</code></p>"+
                     "<p><small>Lehet, hogy további részletek olvashatók a hibáról a böngésző fejlesztői konzolján.</small></p>"
